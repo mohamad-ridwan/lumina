5e1fea192540305ffaab782b07fb0180be3f9b30
mohamad-ridwan <mr643062@gmail.com>
2025-03-30 16:55:03 +0700

    create loading search username users

--- src/sections/chat/AddNewChat.vue
+++ src/sections/chat/AddNewChat.vue
@@ -1,10 +1,20 @@
 <script setup>
 import Input from '@/components/Input.vue';
+import { fetchSearchUsers } from '@/services/api/users';
+import { usersStore } from '@/stores/users';
 import ConfirmPopup from 'primevue/confirmpopup';
 import { ref, watch } from 'vue';
 
+// store
+// profile store
+const userStore = usersStore()
+const { profile } = userStore
+
+// state
+const loadingSearchUsers = ref(false)
 const searchValue = ref('')
 
+// logic
 function debounce(func, delay) {
   let timeoutId;
   return function (...args) {
@@ -16,10 +26,20 @@ function debounce(func, delay) {
 }
 
 const debouncedSearch = debounce(async (newValue) => {
-  console.log(newValue)
+  if (!newValue) {
+    loadingSearchUsers.value = false
+    return
+  }
+  const usersCurrently = await fetchSearchUsers({
+    username: newValue,
+    senderId: profile?.data?.id
+  })
+  loadingSearchUsers.value = false
 }, 1000);
 
+// hooks rendering
 watch(searchValue, (newValue) => {
+  loadingSearchUsers.value = true
   debouncedSearch(newValue)
 })
 </script>
@@ -29,8 +49,8 @@ watch(searchValue, (newValue) => {
     <template #message>
       <div class="min-w-[250px] p-2.5 flex flex-col gap-2">
         <h1 class="font-bold text-sm text-center">New Chat</h1>
-        <Input v-model="searchValue" class-icon="left-3" icon="pi-search" input-class="!pl-8"
-          placeholder="Search username or number" />
+        <Input v-model="searchValue" class-icon="left-3" :icon="loadingSearchUsers ? 'pi-spin pi-spinner' : 'pi-search'"
+          input-class="!pl-8" placeholder="Search username or number" />
       </div>
     </template>
   </ConfirmPopup>
