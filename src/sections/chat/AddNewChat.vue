4cc67ec5e4f6efd36945096d7d887d5d2fdf06e8
mohamad-ridwan <mr643062@gmail.com>
2025-03-30 16:38:43 +0700

    create fetchSearchUsers api & debounce search

--- src/sections/chat/AddNewChat.vue
+++ src/sections/chat/AddNewChat.vue
@@ -1,9 +1,27 @@
 <script setup>
 import Input from '@/components/Input.vue';
 import ConfirmPopup from 'primevue/confirmpopup';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const searchValue = ref('')
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+const debouncedSearch = debounce(async (newValue) => {
+  console.log(newValue)
+}, 1000);
+
+watch(searchValue, (newValue) => {
+  debouncedSearch(newValue)
+})
 </script>
 
 <template>
--- src/services/api/users.js
+++ src/services/api/users.js
@@ -1,6 +1,18 @@
 import { clientUrl } from '../apiBaseUrl'
 import { fetchData } from '../fetchData'
 
+export const fetchSearchUsers = async (req) => {
+  const result = await fetchData(`${clientUrl}/users/search-users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+
+  return result
+}
+
 export const fetchProfile = async (req) => {
   const result = await fetchData(`${clientUrl}/users/profile`, {
     method: 'POST',
