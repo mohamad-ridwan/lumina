0aed9b76856ce7c1fa972efe4058f77e0299fa73
mohamad-ridwan <mr643062@gmail.com>
2025-03-30 19:47:43 +0700

    fix: chat color message

--- src/sections/chat-room/RecipientMessage.vue
+++ src/sections/chat-room/RecipientMessage.vue
@@ -4,8 +4,8 @@ const { textMessage } = defineProps(['textMessage'])
 
 <template>
   <div class="flex flex-col gap-1 pb-2">
-    <div class="bg-[#d1fae5] rounded-lg p-2 max-w-xs self-start" style="box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);">
-      <p>{{ textMessage }}</p>
+    <div class="bg-[#f1f1f1] rounded-lg p-2 max-w-xs self-start" style="box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);">
+      <p class="text-sm">{{ textMessage }}</p>
     </div>
     <span class="text-xs text-[#111827] self-start">10:00 AM</span>
   </div>
--- src/sections/chat-room/SenderMessage.vue
+++ src/sections/chat-room/SenderMessage.vue
@@ -4,8 +4,8 @@ const { textMessage } = defineProps(['textMessage'])
 
 <template>
   <div class="flex flex-col items-end gap-1 pb-2">
-    <div class="bg-[#e0f2fe] rounded-lg p-2 max-w-xs self-end" style="box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);">
-      <p>{{ textMessage }}</p>
+    <div class="bg-[#2e74e8] rounded-lg p-2 max-w-xs self-end" style="box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);">
+      <p class="text-white text-sm">{{ textMessage }}</p>
     </div>
     <span class="text-xs text-[#111827] self-end">10:01 AM</span>
   </div>
