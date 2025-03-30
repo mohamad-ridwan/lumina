49b7e200924437dec2b535b9dadec7c34d9ad6b6
mohamad-ridwan <mr643062@gmail.com>
2025-03-31 02:06:03 +0700

    fix: responsive auth layout

--- src/layout/auth/AuthLayout.vue
+++ src/layout/auth/AuthLayout.vue
@@ -11,8 +11,8 @@ const { title, desc } = defineProps({
 
 <template>
   <div class="flex min-h-screen">
-    <div class="flex w-[50%] justify-center">
-      <div class="min-w-[350px] pt-[10%] pb-[5%] flex flex-col gap-6">
+    <div class="flex w-full md:w-[50%] justify-center">
+      <div class="w-[80%] md:min-w-[350px] pt-[10%] pb-[5%] flex flex-col gap-6">
         <div class="flex flex-col gap-2">
           <img src="/public/logo.png" alt="" class="max-w-[100px] object-cover">
           <h1 class="font-bold text-2xl">{{ title }}</h1>
@@ -23,7 +23,7 @@ const { title, desc } = defineProps({
         </div>
       </div>
     </div>
-    <div class="flex w-[50%] justify-center">
+    <div class="hidden w-[50%] justify-center md:flex">
       <div :class="`w-full flex bg-[#2e74e8] justify-center items-center`">
         <img src="../../assets/images/auth.png" class="w-full max-w-[600px] object-contain" alt="">
       </div>
