a61e6abe62946c69d02459e750a428d1a71fb20e
mohamad-ridwan <mr643062@gmail.com>
2025-03-30 20:32:00 +0700

    create open chat room functionality

--- src/sections/chat/chat-item/ChatItem.vue
+++ src/sections/chat/chat-item/ChatItem.vue
@@ -1,7 +1,9 @@
 <script setup>
 // import
 import ChatProfile from '@/components/ChatProfile.vue';
+import { fetchChatRoom } from '@/services/api/chat-room';
 import { socket } from '@/services/socket/socket';
+import { useChatRoomStore } from '@/stores/chat-room';
 import { usersStore } from '@/stores/users';
 import { onBeforeMount, onMounted, ref } from 'vue';
 
@@ -12,11 +14,26 @@ const { item } = defineProps(['item'])
 // profile store
 const userStore = usersStore()
 const { profile, profileIdConnection } = userStore
+// chat-room store
+const chatRoomStore = useChatRoomStore()
+const { setChatRoom } = chatRoomStore
 
 // state
 const name = ref('')
 
+// logic
 const userIdCurrently = item.userIds.filter(id => id !== profile.data.id)?.[0]
+
+const handleClickUser = async () => {
+  // get chat room
+  const chatRoomCurrently = await fetchChatRoom({
+    userIds: item.userIds,
+    mainUserId: profile.data.id
+  })
+  if (chatRoomCurrently?.data) {
+    setChatRoom(chatRoomCurrently)
+  }
+}
 
 // hooks rendering
 onBeforeMount(() => {
@@ -49,5 +66,5 @@ onMounted(() => {
 
 <template>
   <ChatProfile :username="name" :from-me="item.latestMessage.senderUserId === profile?.data?.id"
-    :text-message="item.latestMessage.textMessage" />
+    :text-message="item.latestMessage.textMessage" @click="handleClickUser" />
 </template>
