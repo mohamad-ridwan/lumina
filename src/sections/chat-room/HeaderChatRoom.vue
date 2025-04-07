215ed00813f0f3eef3e9e9abbb2e3290196f9fba
mohamad-ridwan <mr643062@gmail.com>
2025-04-08 01:47:48 +0700

    fix: padding header chat room

--- src/sections/chat-room/HeaderChatRoom.vue
+++ src/sections/chat-room/HeaderChatRoom.vue
@@ -78,7 +78,7 @@ function handleBack() {
 </script>
 
 <template>
-  <header class="bg-white p-4 border-b-[#f1f1f1] border-b-[1px] flex items-center gap-4">
+  <header class="bg-white px-4 py-3 border-b-[#f1f1f1] border-b-[1px] flex items-center gap-4">
     <Button icon="pi pi-angle-left" aria-label="Back"
       class="!rounded-full !bg-transparent hover:!bg-transparent !h-[25px] !w-[25px] justify-center items-center flex cursor-pointer !text-black !outline-none !border-none !p-0"
       size="large" icon-class="!text-lg" @click="handleBack" />
