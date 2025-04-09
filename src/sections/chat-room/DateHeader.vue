2e259605706dd99d30b0840ecea56d515503cd56
mohamad-ridwan <mr643062@gmail.com>
2025-04-09 21:21:27 +0700

    fix: date header messaegs

--- src/sections/chat-room/DateHeader.vue
+++ src/sections/chat-room/DateHeader.vue
@@ -5,7 +5,7 @@ const { date } = defineProps(['date'])
 </script>
 
 <template>
-  <div class="sticky bottom-0 flex justify-center items-center z-50 py-2">
+  <div class="flex justify-center items-center">
     <div class="bg-[#e0e0e0] px-2 py-1 rounded-md flex justify-center items-center">
       <span class="font-bold text-xs rotate-180">{{ date }}</span>
     </div>
--- src/sections/chat-room/RoomView.vue
+++ src/sections/chat-room/RoomView.vue
@@ -4,7 +4,7 @@ import FooterChatRoom from './FooterChatRoom.vue';
 import HeaderChatRoom from './HeaderChatRoom.vue';
 import SenderMessage from './SenderMessage.vue';
 import RecipientMessage from './RecipientMessage.vue';
-import { computed, onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue';
+import { computed, nextTick, onBeforeUnmount, onMounted, onUnmounted, ref, watch } from 'vue';
 import { socket } from '@/services/socket/socket';
 // import SpamMessage from '@/spam-message/SpamMessage.vue';
 import { useChatRoomStore } from '@/stores/chat-room';
@@ -38,6 +38,12 @@ const { chatRoom, chatRoomMessages } = storeToRefs(chatRoomStore)
 const scroller = ref(null)
 const SCROLL_THRESHOLD = 200;
 const showScrollDownButton = ref(false);
+const headerRefs = ref({})
+const observer = ref(null)
+const currentStickyHeader = ref({
+  latestMessageTimestamp: 0,
+  text: ''
+})
 
 // logic
 const memoizedChatRoomId = computed(() => {
@@ -63,20 +69,31 @@ const memoizedChatRoomDataWithHeaders = computed(() => {
     if (!groupedMessages.has(formattedDate)) {
       groupedMessages.set(formattedDate, { date: itemDate, items: [] });
     }
+
     groupedMessages.get(formattedDate)?.items.push(item);
   }
 
   const result = [];
+
   for (const [formattedDate, group] of groupedMessages) {
-    result.push(...group.items); // push messages first
-    const messageId = generateRandomId(15)
+    const messages = group.items;
+
+    if (!messages.length) continue;
+    const lastMessage = messages[messages.length - 1];
+
+    // Tambahkan pesan-pesan
+    result.push(...messages);
+
+    // Tambahkan header terakhir (untuk penanda grouping)
+    const endMessageId = generateRandomId(15);
     result.push({
-      id: messageId,
-      messageId: messageId,
+      id: endMessageId,
+      messageId: endMessageId,
       isHeader: true,
+      isEndHeader: true,
       headerText: formattedDate,
-      messageId: `header-${formattedDate}`
-    }); // then push header
+      latestMessageTimestamp: lastMessage.latestMessageTimestamp, // akhir grup
+    });
   }
 
   return result;
@@ -119,7 +136,74 @@ function handleBeforeUnload() {
   }
 }
 
-// hooks rendering
+function setHeaderRef(el, item) {
+  if (!item?.latestMessageTimestamp) return
+
+  const key = item.id // bisa juga pakai uniqueKey kalau ada
+  if (el) {
+    headerRefs.value[key] = el
+  } else {
+    delete headerRefs.value[key]
+  }
+}
+
+const onScroll = () => {
+  const containerRect = scroller.value?.$el?.getBoundingClientRect()
+  if (!containerRect) return
+
+  const containerBottom = containerRect.bottom // karena rotate 180
+  const itemElements = Object.values(headerRefs.value || {}) // semua item, bukan cuma header
+
+  let closestItem = null
+  let minDistance = Infinity
+
+  itemElements.forEach((el) => {
+    const rect = el.getBoundingClientRect()
+    const visualBottom = rect.bottom + 580
+    const distance = Math.abs(visualBottom - containerBottom)
+
+    const isAboveViewport = visualBottom <= containerBottom - 30
+    const timestamp = Number(el.dataset.timestamp)
+
+    if (isAboveViewport && distance < minDistance && !isNaN(timestamp)) {
+      minDistance = distance
+      closestItem = { timestamp, el }
+    }
+  })
+
+  if (closestItem && closestItem.timestamp !== currentStickyHeader.value.latestMessageTimestamp) {
+    const itemDate = dayjs(closestItem.timestamp).startOf('day')
+    currentStickyHeader.value = {
+      latestMessageTimestamp: closestItem.timestamp,
+      text: formatDate(itemDate),
+    }
+  }
+}
+
+watch(() => memoizedChatRoomDataWithHeaders.value, (data) => {
+  if (data.length === 0) {
+    currentStickyHeader.value = {
+      latestMessageTimestamp: 0,
+      text: ''
+    }
+    headerRefs.value = {}
+  }
+  nextTick(() => {
+    Object.values(headerRefs.value).forEach((el) => {
+      if (el) observer.value?.observe(el)
+    })
+    onScroll()
+  })
+})
+
+watch(
+  () => scroller.value,
+  () => {
+    onScroll()
+  },
+  { immediate: true }
+)
+
 onMounted(() => {
   window.addEventListener('beforeunload', handleBeforeUnload);
 });
@@ -175,6 +259,9 @@ onMounted(() => {
 const handleScroll = () => {
   if (scroller.value?.$el) {
     const scrollTop = scroller.value.$el.scrollTop;
+    nextTick(() => {
+      onScroll()
+    })
     showScrollDownButton.value = scrollTop > SCROLL_THRESHOLD;
   }
 };
@@ -191,26 +278,38 @@ onUnmounted(() => {
 
 <template>
   <!-- <SpamMessage /> -->
-  <div class="flex flex-col h-screen border-l-[#f1f1f1] border-l-[1px]">
+  <div class="flex flex-col h-screen border-l-[#f1f1f1] border-l-[1px] relative">
     <HeaderChatRoom :recipient-id="memoizedUserIds.filter(id => id !== profile.data.id)?.[0]"
       :profile-id="profile.data.id" :profile-id-connection="profileIdConnection" />
 
-    <DynamicScroller ref="scroller" :items="memoizedChatRoomDataWithHeaders" :min-item-size="54"
-      class="flex-1 !p-4 space-y-2 bg-[#f9fafb]"
+    <div v-if="currentStickyHeader.text"
+      class="absolute top-20 z-10 rotate-180 flex justify-center items-center w-full">
+      <DateHeader :date="currentStickyHeader.text" />
+    </div>
+
+    <DynamicScroller id="scrollChatRoom" ref="scroller" :items="memoizedChatRoomDataWithHeaders" :min-item-size="54"
+      class="flex-1 !p-4 space-y-2 bg-[#f9fafb]" :buffer="0" :page-mode="false"
       style="display: flex; flex-direction: column; transform: rotate(180deg); direction: rtl;">
       <template v-slot="{ item, index, active }">
-        <DateHeader v-if="item?.isHeader" :date="item.headerText" />
         <DynamicScrollerItem :item="item" :active="active" :size-dependencies="[
           item.textMessage,
         ]" :data-index="index" :key="item.messageId">
-          <SenderMessage v-if="!item?.isHeader && item.senderUserId === profile.data.id"
-            :text-message="item.textMessage" :latest-message-timestamp="item.latestMessageTimestamp"
-            :status="item.status" :message-id="item.messageId" />
-          <RecipientMessage v-if="!item?.isHeader && item.senderUserId !== profile.data.id"
-            :text-message="item.textMessage" :latest-message-timestamp="item.latestMessageTimestamp"
-            :status="item.status" :chat-id="memoizedChatId" :chat-room-id="memoizedChatRoomId"
-            :message-id="item.messageId" />
-
+          <div v-if="item?.isHeader">
+            <DateHeader v-if="item?.isEndHeader" :date="item.headerText" />
+          </div>
+          <div :id="`${item.id}-${item.latestMessageTimestamp}`" :ref="(el) => setHeaderRef(el, item)"
+            :data-timestamp="item.latestMessageTimestamp">
+            <SenderMessage v-if="!item?.isHeader && item.senderUserId === profile.data.id"
+              :text-message="item.textMessage" :latest-message-timestamp="item.latestMessageTimestamp"
+              :status="item.status" :message-id="item.messageId" />
+          </div>
+          <div :id="`${item.id}-${item.latestMessageTimestamp}`" :ref="(el) => setHeaderRef(el, item)"
+            :data-timestamp="item.latestMessageTimestamp">
+            <RecipientMessage v-if="!item?.isHeader && item.senderUserId !== profile.data.id"
+              :text-message="item.textMessage" :latest-message-timestamp="item.latestMessageTimestamp"
+              :status="item.status" :chat-id="memoizedChatId" :chat-room-id="memoizedChatRoomId"
+              :message-id="item.messageId" />
+          </div>
         </DynamicScrollerItem>
       </template>
     </DynamicScroller>
