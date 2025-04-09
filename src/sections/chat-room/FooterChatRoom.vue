2f59bb049bea35aa23d6383209bfd30c9dabd678
mohamad-ridwan <mr643062@gmail.com>
2025-04-09 23:24:10 +0700

    fix: type enter to submit message

--- src/sections/chat-room/FooterChatRoom.vue
+++ src/sections/chat-room/FooterChatRoom.vue
@@ -43,6 +43,19 @@ const onFormSubmit = () => {
     initialValues.value.textMessage = ''
   }
 };
+
+const handleTextareaKeydown = (event) => {
+  if (event.key === 'Enter' && event.shiftKey) {
+    // Shift + Enter: Sisipkan baris baru
+    initialValues.value.textMessage += '\n';
+    // Mencegah perilaku default Enter (submit jika tidak ada preventDefault di form)
+    event.preventDefault();
+  } else if (event.key === 'Enter') {
+    // Enter saja: Submit pesan
+    event.preventDefault(); // Mencegah baris baru
+    onFormSubmit();
+  }
+};
 </script>
 
 <template>
@@ -50,7 +63,7 @@ const onFormSubmit = () => {
     <Form :initialValues="initialValues" @submit="onFormSubmit" class="flex items-end w-full gap-2">
       <Textarea v-model="initialValues.textMessage" rows="1" cols="20"
         class="!text-sm flex-1 rounded-l-md p-2 bg-[#f1f1f1] min-h-[38px] max-h-[150px] w-full !overflow-y-auto"
-        placeholder="Type Message..." name="textMessage" :autoResize="true" />
+        placeholder="Type Message..." name="textMessage" :autoResize="true" @keydown="handleTextareaKeydown" />
       <Button icon="pi pi-send" aria-label="Send Message"
         class="!rounded-full !bg-[#2e74e8] hover:!bg-[#2e74e8] !h-[35px] !w-[35px] justify-center items-center flex cursor-pointer !text-white !outline-none !border-none !p-0"
         size="large" icon-class="!text-[16px] !mr-0.5 !mt-0.5" type="submit" />
