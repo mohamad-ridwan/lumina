fbe3327651eb3a93e0af17c8a76a64fa6f233eaf
mohamad-ridwan <mr643062@gmail.com>
2025-04-11 16:49:51 +0700

    added public api server

--- src/services/apiBaseUrl.js
+++ src/services/apiBaseUrl.js
@@ -1,1 +1,3 @@
-export const clientUrl = 'http://localhost:4001'
+// export const clientUrl = 'http://localhost:4001'
+// export const clientUrl = 'https://be-lumina.vercel.app'
+export const clientUrl = 'https://be-lumina-production.up.railway.app'
--- src/services/socket/socket.js
+++ src/services/socket/socket.js
@@ -1,6 +1,8 @@
 // socket.js
 import { io } from 'socket.io-client'
+import { clientUrl } from '../apiBaseUrl'
 
-const URL = 'http://localhost:4001' // Ganti dengan URL server socket Anda
+// const URL = 'http://localhost:4001' // Ganti dengan URL server socket Anda
+const URL = clientUrl // Ganti dengan URL server socket Anda
 
 export const socket = io(URL)
