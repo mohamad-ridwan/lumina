80127db43d95d63cff31839a053ca7f11f1df1f2
mohamad-ridwan <mr643062@gmail.com>
2025-05-26 12:06:46 +0700

    fix: update production URL for client API endpoint

--- src/services/apiBaseUrl.js
+++ src/services/apiBaseUrl.js
@@ -1,8 +1,13 @@
 // export const clientUrl = 'http://localhost:4001'
 // export const clientUrl = 'https://be-lumina.vercel.app'
+// export const clientUrl =
+//   import.meta.env.MODE === 'development'
+//     ? 'http://localhost:4001'
+//     : 'https://be-lumina-production.up.railway.app'
+
 export const clientUrl =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:4001'
-    : 'https://be-lumina-production.up.railway.app'
+    : 'https://be-lumina-production-ee26.up.railway.app'
 
 // export const clientUrl = 'https://be-lumina-production.up.railway.app'
