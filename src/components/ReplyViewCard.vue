fadd1bd2d118873d285af9b449940e750561e5f7
mohamad-ridwan <mr643062@gmail.com>
2025-05-29 21:11:56 +0700

    fix: replace img with v-lazy-image for optimized image loading in ReplyViewCard

--- src/components/ReplyViewCard.vue
+++ src/components/ReplyViewCard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { computed } from 'vue'
+import VLazyImage from "v-lazy-image";
 
 const { usernameClass, fromMessageUsername, wrapperClass, textMessage, textMessageClass, wrapperStyle, document } = defineProps(['usernameClass', 'fromMessageUsername', 'wrapperClass', 'textMessage', 'textMessageClass', 'wrapperStyle', 'document'])
 
@@ -16,8 +17,10 @@ const memoizedTextMessage = computed(() => {
   <div :class="`w-full flex items-start justify-between gap-2 pl-2 cursor-pointer ${wrapperClass}`"
     :style="wrapperStyle" @click="emits('onClick')">
     <div class="flex items-center gap-2">
-      <div v-if="document?.type === 'image'"><img :src="document.url" alt="reply"
-          class="h-10 w-15 max-w-full object-contain rounded-sm"></div>
+      <div v-if="document?.type === 'image'">
+        <v-lazy-image :src="`${document?.url}`" alt="reply" :src-placeholder="document?.thumbnail"
+          class="h-10 w-15 max-w-full object-contain rounded-sm" sizes="(max-width: 60px) 40px, 60px" />
+      </div>
       <div class="flex flex-col w-full pr-2">
         <h1 :class="`${usernameClass} font-semibold flex`">
           {{ fromMessageUsername }}
@@ -27,3 +30,14 @@ const memoizedTextMessage = computed(() => {
     </div>
   </div>
 </template>
+
+<style scoped>
+.v-lazy-image {
+  filter: blur(10px);
+  transition: filter 0.1s;
+}
+
+.v-lazy-image-loaded {
+  filter: blur(0);
+}
+</style>
